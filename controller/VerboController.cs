2be62bfac72ac1f92b8a9787f161c1930ec422b8
LuisGuilhermeCipriani <cipriani.luisguilherme@gmail.com>
2020-07-28 21:08:18 -0300

    Criação da Classe VerboController

---
+++ controller/VerboController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memorizacao.controller
+{
+    class VerboController
+    {
+        private Verbo verbo;
+        private List<Verbo> listaVerbos;
+        private List<Verbo> listaCorretos;
+        private List<Verbo> listaIncorretos;
+
+        public VerboController()
+        {
+            verbo = new Verbo();
+            listaVerbos = new List<Verbo>();
+            listaCorretos = new List<Verbo>();
+            listaIncorretos = new List<Verbo>();
+        }
+
+        public Verbo getVerbo()
+        {
+            return verbo;
+        }
+
+        public List<Verbo> getListaVerbos()
+        {
+            return listaVerbos;
+        }
+
+        public List<Verbo> getlistaCorretos()
+        {
+            return listaCorretos;
+        }
+
+        public List<Verbo> getlistaIncorretos()
+        {
+            return listaIncorretos;
+        }
+
+
+    }
+}
